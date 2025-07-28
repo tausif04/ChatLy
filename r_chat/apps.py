7d502bce0fdc06571cffd1978896f138d389d8b3
Md Tausif Bin Mozid <146369899+tausif04@users.noreply.github.com>
2025-07-28 15:25:46 +0600

    Delete r_chat directory

--- r_chat/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class RChatConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'r_chat'
--- r_chat/views.py
+++
@@ -1,4 +0,0 @@
-from django.shortcuts import render
-
-def chat_view(request):
-    return render(request,'r_chat/chat.html')
